e59b6010cf4a6a4d49dc395012a950040c0027da
darserg <darserg1212@mail.ru>
2026-02-27 11:13:14 +0400

    fix: created basic json

--- fastapi_app/src/api/base.py
+++ fastapi_app/src/api/base.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, HTTPException
 
-from schemas.posts import Post, PostBase
+from schemas.posts import Post
 
 
 router = APIRouter()
@@ -13,7 +13,7 @@ async def get_hello_world() -> dict:
     return response
 
 
-@router.post("/test_json", status_code=status.HTTP_201_CREATED, response_model=PostBase)
+@router.post("/test_json", status_code=status.HTTP_201_CREATED, response_model=Post)
 async def test_json(post: Post) -> dict:
     if len(post.text) < 3:
         raise HTTPException(
@@ -22,8 +22,20 @@ async def test_json(post: Post) -> dict:
         )
 
     response = {
-        "post_text": post.text,
-        "author_name": post.author.login
+        "id": 0,
+        "author": {
+            "login": "string",
+            "id": 0,
+            "created_at": "2026-02-27T07:00:40.369Z"
+        },
+        "category": {
+            "title": "string",
+            "description": "string",
+            "is_published": "true"
+        },
+        "text": "string",
+        "datetime_to_publish": "2026-02-27T07:00:40.369Z",
+        "created_at": "2026-02-27T07:00:40.369Z"
     }
 
     return Post.model_validate(obj=response)
